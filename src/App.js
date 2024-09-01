ad65861887e6f01060223b9c75e5edcd522d3e63
seonghyunkim <80308536+ilikekimchi@users.noreply.github.com>
2024-09-01 23:42:12 +0900

    [20240902] 테스트 두번째임

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/about">About</Link></li>
+          <li><Link to="/about">fassz</Link></li>
         </ul>
       </nav>
 
